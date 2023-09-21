4d68993dea6415426a97787437a79b747c47d987
donnie <donnie4w@gmail.com>
2023-09-21 14:36:21 +0800

    Update HttpClient.java

--- src/main/java/io/github/donnie4w/tlmq/cli/HttpClient.java
+++ src/main/java/io/github/donnie4w/tlmq/cli/HttpClient.java
@@ -7,86 +7,41 @@
 package io.github.donnie4w.tlmq.cli;
 
 import java.io.IOException;
-import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
 import java.util.logging.Logger;
+
 import org.apache.http.Header;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.config.Registry;
-import org.apache.http.config.RegistryBuilder;
-import org.apache.http.conn.socket.ConnectionSocketFactory;
-import org.apache.http.conn.socket.PlainConnectionSocketFactory;
-import org.apache.http.conn.ssl.NoopHostnameVerifier;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
 import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.http.message.BasicHeader;
-import org.apache.http.ssl.SSLContextBuilder;
 import org.apache.http.util.EntityUtils;
-
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
 
 public class HttpClient {
     public static Logger logger = Logger.getLogger("HttpClient");
-    private static PoolingHttpClientConnectionManager poolConnManager = null;
 
-    static {
-        try {
-            SSLContext ctx = SSLContext.getInstance("TLSv1.2");
-            X509TrustManager tm = new X509TrustManager() {
-                @Override
-                public X509Certificate[] getAcceptedIssuers() {
-                    return null;
-                }
-                @Override
-                public void checkClientTrusted(X509Certificate[] arg0,
-                                               String arg1) throws CertificateException {
-                }
-                @Override
-                public void checkServerTrusted(X509Certificate[] arg0,
-                                               String arg1) throws CertificateException {
-                }
-            };
-            ctx.init(null, new TrustManager[] { tm }, null);
-            SSLConnectionSocketFactory ssf = new SSLConnectionSocketFactory(
-                    ctx, NoopHostnameVerifier.INSTANCE);
-            SSLContextBuilder builder = new SSLContextBuilder();
-            builder.loadTrustMaterial(null, new TrustSelfSignedStrategy());
-            Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory>create().register("http", PlainConnectionSocketFactory.getSocketFactory()).register("https", ssf).build();
-            poolConnManager = new PoolingHttpClientConnectionManager(socketFactoryRegistry);
-            poolConnManager.setMaxTotal(50);
-        } catch (Exception e) {
-           throw new TlRunTimeException(e);
-        }
-    }
 
-    public static CloseableHttpClient getHttpClient() {
-        RequestConfig config = RequestConfig.custom().setConnectTimeout(5000)
-                .setSocketTimeout(10000)
-                .build();
-        CloseableHttpClient httpClient = HttpClients.custom()
-                .setConnectionManager(poolConnManager)
+    public static CloseableHttpClient getHttpClient() throws NoSuchAlgorithmException, KeyManagementException {
+        RequestConfig config = RequestConfig.custom().setConnectionRequestTimeout(5000).build();
+        CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(SSLUtil.newSSLConnectionSocketFactory())
                 .setDefaultRequestConfig(config)
                 .build();
         return httpClient;
     }
 
-    public static byte[] post(String uri, byte[] bs,String origin,String auth) throws TlException {
+    public static byte[] post(String uri, byte[] bs, String origin, String auth) throws TlException {
         HttpPost httpPost = new HttpPost(uri);
         CloseableHttpResponse response = null;
         try {
             httpPost.setEntity(new ByteArrayEntity(bs));
-            httpPost.setHeaders(new Header[]{new BasicHeader("Origin", origin),new BasicHeader("Cookie", "auth="+auth)});
+            httpPost.setHeaders(new Header[]{new BasicHeader("Origin", origin), new BasicHeader("Cookie", "auth=" + auth)});
             response = getHttpClient().execute(httpPost);
             return EntityUtils.toByteArray(response.getEntity());
-        } catch (IOException e) {
+        } catch (Exception e) {
             throw new TlException(e);
         } finally {
             try {
